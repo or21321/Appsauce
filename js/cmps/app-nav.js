4646131a26e425f1c174db88edb262c287b418b0
Or Hadar <ormoglob@gmail.com>
2021-06-26 21:56:48 +0300

    before final

--- js/cmps/app-nav.js
+++ js/cmps/app-nav.js
@@ -1,23 +1,21 @@
 export default {
     template: `
-    <section>
         <div @click="toggleNavModal" class="app-nav">
             ☰
             <div v-if="isNavModalOpen" class="nav-modal">
                 <div @click="$router.push('/email/inbox')" class="email">
                     <!-- <span class="material-icons">mail</span> -->
                     <img src='../../img/icons/best-email.png'>
-                    <span>email</span>
+                    <span>Email</span>
                     <!-- <img src='../../img/icons/email.png'> -->
                 </div>
                 <div @click="$router.push('/keep')" class="keep">
                     <img src='../../img/icons/keep.png'>
-                    <span>keep</span>
+                    <span>Keep</span>
                     <!-- <span class="material-icons">note</span> -->
                 </div>
             </div>
         </div>
-    </section>
     `,
     data() {
         return {
